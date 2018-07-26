14ec69e8abf1e56e97428d1d0f7131209710fa77
mkm1997 <mmanish184@gmail.com>
2018-07-26 12:19:07 +0530

    hack

--- long_lang_path/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LongLangPathConfig(AppConfig):
-    name = 'long_lang_path'
--- long_lang_path/urls.py
+++
@@ -1,9 +0,0 @@
-#from django.contrib import admin
-from django.urls import path, re_path, include
-from . import views
-
-urlpatterns = [
-
-
-    re_path("api/",views.index,name="index")
-]
